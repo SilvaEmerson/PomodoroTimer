dd817cf159d6c9e32b26c7f7eaa1e810fda73cfc
SilvaEmerson <emersonjose877@gmail.com>
2018-08-24 16:20:53 -0300

    Add input area to edit work time

--- src/script.js
+++ src/script.js
@@ -1,14 +1,66 @@
-import {preState} from './index/index';
+import {preState, getElement} from './index/index';
 import {
     sprint,
-    actionTimeWithBigHalf,
-    actionTimeWithNormalHalf,
-    initMessage
+    initMessage,
+    behavior,
+    constructTimer,
+    createSequence,
 } from './timer/timer';
 
 
-const state = preState(
-    sprint, initMessage, actionTimeWithNormalHalf, actionTimeWithBigHalf
-);
+const actionTimeWithNormalHalf = (time, timeInterval) => {
+    let workConfig = {
+        factor: 'minutes',
+        time: time,
+        message: 'º sprint terminated',
+    };
 
-const onTap = state(document);
+    let intervalConfig = {
+        factor: 'minutes',
+        time: timeInterval,
+        message: 'º interval terminated',
+    };
+
+    return createSequence(sprint,
+        constructTimer(behavior(workConfig)),
+        constructTimer(behavior(intervalConfig))
+    );
+};
+
+const actionTimeWithBigHalf = actionTimeWithNormalHalf;
+
+const workTime = getElement(document, '#workTime');
+const restTime = getElement(document, '#restTime');
+const confirmConfigBtn = getElement(document, '#confirmConfig');
+const biggestRestTime = getElement(document, '#biggestRestTime');
+const startPomodoroBtn = getElement(document, '#startPomodoro');
+
+
+const addListener = ({element, event, action}) => {
+    element[event] = action;
+};
+
+addListener({
+    element: confirmConfigBtn,
+    action: () => {
+        const state = preState(
+            sprint,
+            initMessage,
+            actionTimeWithNormalHalf(
+                workTime.value, restTime.value
+            ),
+            actionTimeWithBigHalf(
+                workTime.value, biggestRestTime.value
+            )
+        );
+
+        const onTap = state(document);
+        startPomodoroBtn.style.visibility = 'visible';
+        addListener({
+            element: startPomodoroBtn,
+            event: 'onclick',
+            action: onTap,
+        });
+    },
+    event: 'onclick',
+});
